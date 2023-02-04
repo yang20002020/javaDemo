e3d5cb0729673d3122653379b8d6833a1a9f5723
yang20002020 <2628636987@qq.com>
2023-02-04 23:46:46 +0800

    exception

---
+++ 6_exception/scoreException.java
@@ -0,0 +1,10 @@
+package itheima;
+
+public class scoreException extends  Exception{
+    public scoreException() {
+    }
+
+    public scoreException(String message) {
+        super(message);
+    }
+}
---
+++ 6_exception/test.java
@@ -0,0 +1,21 @@
+package itheima;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args)  {
+       Scanner sc= new Scanner(System.in);
+       System.out.println("请输入分数");
+       int score=sc.nextInt();
+       Teacher t=new Teacher();
+        try {
+            t.checkScore(score);
+        } catch (scoreException e) {
+          //  throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+
+    }
+}
