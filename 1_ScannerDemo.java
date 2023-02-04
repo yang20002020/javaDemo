aa07ef699d8b184b42ea9b706c83a9761dc13d1e
yang20002020 <2628636987@qq.com>
2023-02-04 23:37:31 +0800

    scanner

---
+++ 1_ScannerDemo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+//注意 文件名和类名要保持一致
+public class  ScannerDemo_1 {
+   public  static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //接受数据
+        int x=sc.nextInt();
+        //输出
+        System.out.println(x);
+   }
+
+}
