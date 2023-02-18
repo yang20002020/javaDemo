d2f3aec4ac4f1bd29401964d8be086e386449f22
yang20002020 <2628636987@qq.com>
2023-02-18 16:14:02 +0800

    predicate接口

---
+++ 35_predicate接口/test.java
@@ -0,0 +1,21 @@
+package it_09;
+
+import java.util.function.Predicate;
+
+/*
+predicate <T>接口通常用于判断参数，是否满足指定的条件
+ */
+public class test {
+    public static void main(String[] args) {
+        boolean b1=checkString("hello",(String s)->{
+            return s.length()>8;
+        });
+        System.out.println(b1);
+        boolean b2=checkString("hello", s-> s.length()>8);
+        System.out.println(b2);
+    }
+    //判断给定的字符串是否满足要求
+    private static boolean checkString (String s, Predicate<String> pre){
+        return pre.test(s);
+    }
+}
