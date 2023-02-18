8e6e5fff74a0e127b3c6c2ebc87b51d45fe07d5a
yang20002020 <2628636987@qq.com>
2023-02-18 16:30:35 +0800

    preidcate接口

--- 35_predicate接口/test.java
+++ 35_predicate接口/test.java
@@ -13,9 +13,16 @@ public class test {
         System.out.println(b1);
         boolean b2=checkString("hello", s-> s.length()>8);
         System.out.println(b2);
+        boolean b3=checkString1("hello", s-> s.length()>8);
+        System.out.println(b3);
     }
     //判断给定的字符串是否满足要求
     private static boolean checkString (String s, Predicate<String> pre){
         return pre.test(s);
+    }
+
+    private static boolean checkString1 (String s, Predicate<String> pre){
+        //return !pre.test(s);
+        return pre.negate().test(s);
     }
 }
