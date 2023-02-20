dee3fd3e6214c013f754f5759029235672a26afd
yang20002020 <2628636987@qq.com>
2023-02-20 17:04:12 +0800

    stream流案例

---
+++ 48_stream流/Actor.java
@@ -0,0 +1,15 @@
+package it_15;
+
+public class Actor {
+    private String name;
+    public Actor(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name=name;
+    }
+
+}
