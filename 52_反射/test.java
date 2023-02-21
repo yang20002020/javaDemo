54835ec4143761ee60fe0d690b23ae84a395ae0f
yang20002020 <2628636987@qq.com>
2023-02-21 18:20:48 +0800

    反射

---
+++ 52_反射/test.java
@@ -0,0 +1,33 @@
+package it_16;
+
+
+/*
+三种方式获得class对象
+1.使用类的class属性来获得该类对应的class对象，举例:Student.class 将返回student类对应的class对象
+2.调用对象的getClass()方法，返回该对象所属类对应的Class对象
+ 该方法是object类中的方法，所有java对象都可以调用该方法
+3.使用Class类中的静态方法forName(String className),该方法需要传入字符串参数，
+ 该字符串参数的值是某个类的全部路径，也就是完整包名的路径
+ */
+public class test {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //使用类的class属性来获取该类对应的class对象
+        Class<Student> c1=Student.class;
+        System.out.println(c1);
+        Class<Student> c2=Student.class;
+        System.out.println(c2);
+        System.out.println(c1==c2);
+        System.out.println("***************");
+        //调用对象的getClass()方法，返回该对象所属类对应的Class对象
+        Student s=new Student();
+        Class<? extends Student>c3=s.getClass();
+        System.out.println(c3);
+        System.out.println(c1==c3);
+        System.out.println("***************");
+        //使用Class类中的静态方法forName(String className)
+        Class<?> c4 = Class.forName("it_16.Student");
+        System.out.println(c4);
+        System.out.println(c1==c4);
+
+    }
+}
